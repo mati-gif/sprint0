74658e5fb059c443ca01f4c5014f1d74b7abebaa
mati-gif <matiassoria7700@gmail.com>
2024-07-24 16:35:04 -0300

    creando la clase persona dentro de el package dia_2

--- src/dia_2/Main.java
+++ src/dia_2/Main.java
@@ -1,9 +1,11 @@
 package dia_2;
 
 public class Main {
+
+
+
     public static void main(String[] args) {
-        int numero = 2;
-        System.out.println(numero);
+
 
     }
 }
---
+++ src/dia_2/Persona.java
@@ -0,0 +1,4 @@
+package dia_2;
+
+public class Persona {
+}
