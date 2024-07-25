dd7530d7bb01ea20c063beff47f1ead1edb0d974
mati-gif <matiassoria7700@gmail.com>
2024-07-25 09:34:46 -0300

    creando el objeto persona y persentandolo

--- src/dia_2/Main.java
+++ src/dia_2/Main.java
@@ -6,6 +6,34 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Hello World!");
+        String[] mascotasPersona1 = {"Rex", "Luna"};
+        Persona persona1 = new Persona("Fernando","Perez","22456654",22,1.80,true,mascotasPersona1);
+
+        String[] mascotasPersona2 = {};
+        Persona persona2 = new Persona("Juan","Gonzales","23545698",23,1.78,false,mascotasPersona2);
+
+
+
+
+
+        System.out.println("Presentación de persona1:");
+        //son metodos del objeto persona.Asi mismo estos metodos pertenencen al objeto persona
+        persona1.mostrarPersona();//lllama al metodo mostrarPersona del objeto persona.
+
+//son metodos del objeto persona.Asi mismo estos metodos pertenencen al objeto persona
+        System.out.println("Presentación de persona2:");
+        persona2.mostrarPersona();//lllama al metodo mostrarPersona del objeto persona.
+
+
+
+
+
+//son metodos del objeto persona.Asi mismo estos metodos pertenencen al objeto persona
+//persona.saltar();//lllama al metodo saltar del objeto persona.
+
+
+
 
     }
 }
--- src/dia_2/Persona.java
+++ src/dia_2/Persona.java
@@ -1,4 +1,141 @@
 package dia_2;
 
 public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String dni;
+    private double altura;
+    private Boolean casado;
+    private String[] nombreMascotas;
+
+//    public Persona(String nombre,String apellido,int edad){//creando el constructor Persona , el cosntructor es un metodo porque le pertenece a la clase persona.
+//        //this. es el contexto del objeto que voy  a crear a partir de esta clase y se usa para acceder a las propiedades que estan definidas en la clase persona.
+//        //es decir this es una palabra clave que representa ese objeto específico que acabas de crear.
+//        //ya que esa clase la necesito para crear un objeto.
+//
+//
+//        this.nombre = nombre;  // this.nombre se refiere a la variable de instancia 'nombre' mientras que nombre es el parametro.
+//        //lo uso para ,dentro del objeto persona , hacer el llamado de la variable de instancia. o dicho de otro modo hacer referncia a la variable que estan en la clase Persona.
+//        this.apellido = apellido;
+//        this.edad = edad;
+//
+//
+//    }
+
+//    public Persona() {// Esto es un constructor vacio.
+//
+//    }
+
+    public Persona(String nombre, String apellido, String dni, int edad, double altura, boolean casado,String[] nombreMascotas) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.edad = edad;
+        this.altura = altura;
+        this.casado = casado;
+        this.nombreMascotas = nombreMascotas;
+    }
+
+
+
+//Los métodos saltar() y mostrarPersona() son métodos de instancia de la clase Persona.
+    // Esto significa que son funciones que pueden ser llamadas en cualquier objeto que sea una instancia de la clase Persona.
+
+    // Ambos metodos hacen referencia al objeto actual de la clase Persona a través de la palabra clave this.
+    // Esto significa que cuando llamas a estos métodos en un objeto Persona,
+    // ellos usan los valores de las variables de instancia (nombre, apellido, y edad) de ese objeto en particular.
+    public void mostrarPersona(){
+
+        System.out.println("hola soy " + this.nombre + " " + this.apellido );
+        System.out.println("tengo " + this.edad + " años." + " y mido " + this.altura + " metros de alutra");
+        System.out.println("mi dni es : " + this.dni);
+        System.out.println(this.casado ? "Estoy casado/a." : "No estoy casado/a.");
+
+        if(this.nombreMascotas != null && this.nombreMascotas.length > 0){
+            System.out.println("Mis mascotas son: ");
+            for (int i = 0; i < this.nombreMascotas.length; i++) {
+                System.out.println(this.nombreMascotas[i]);
+            }
+        } else{
+            System.out.println("No tengo mascotas");
+        }
+
+    }
+
+
+
+
+//    public void saltar(){
+//        System.out.println(this.nombre + " Esta saltando");
+//    }
+
+
+
+
+
+
+    //------metodos Setter y Getters-----//
+
+    public void setNombre(String nombre){//setNombre es el nombre del método. Se usa para cambair el valor del atributo nombre del objeto persona.
+        this.nombre = nombre;//this.nombre: Se refiere al atributo nombre del objeto actual. this es una referencia al objeto en el que se está llamando al método.
+
+        //this . es el objeto que esta llamando al metodo setNombre(), y le va a asignar a this.nombre el valor que esta recibiendo por parametro setNombre() que es nombre
+    }
+
+    public String getNombre(){
+
+    return this.nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setCasado(Boolean casado) {
+        this.casado = casado;
+    }
+
+    public Boolean getCasado() {
+        return casado;
+    }
+
+    public void setNombreMascotas(String[] nombreMascotas) {
+        this.nombreMascotas = nombreMascotas;
+    }
+
+    public String[] getNombreMascotas() {
+        return nombreMascotas;
+    }
+
+
 }
